848b2b4fbd2794fa3914cbd6295518138479134a
Din <rivojidin97@gmail.com>
2024-08-13 19:52:32 +0900

    feat: Initial Setup

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { DogService } from './dog.service';
+
+@Controller('dog')
+export class DogController {
+  constructor(private readonly dogService: DogService) {}
+  @Get()
+  public getHello(): string {
+    return this.dogService.getHello();
+  }
+
+  @Get('greet/:id')
+  public introduce(@Param('id') params: any, @Query() query: any): string {
+    console.log('params:', params);
+    console.log('query', query);
+
+    return this.dogService.introduce();
+  }
+}
